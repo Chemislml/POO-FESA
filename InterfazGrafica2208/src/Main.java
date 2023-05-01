944bf16487b490300aaf08ab3d01528e249a5be2
Chemislml <chavinchema@hotmail.com>
2023-04-30 22:49:20 -0600

    Tarea 12: Interfaces gráficas en Java

---
+++ InterfazGrafica2208/src/Main.java
@@ -0,0 +1,10 @@
+    //PEREZ MOLINA JOSE MANUEL 2208
+
+import iu.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Principal ventana = new Principal("Hola mundo AWT!");
+    }
+}
