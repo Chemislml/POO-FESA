997e7238ed987c3ebad63b9fb50ab181da8d3134
Chemislml <chavinchema@hotmail.com>
2023-05-04 23:22:21 -0600

    Actividad extra 3: Layouts Java

---
+++ LayaoutsJava2208/src/Main.java
@@ -0,0 +1,10 @@
+    //PEREZ MOLINA JOSE MANUEL 2208
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+       // Ventana ventana = new Ventana("Layaouts");
+        VentanaGrid ventanaGrid = new VentanaGrid("Ventana Layout Cuadricula");
+    }
+}
