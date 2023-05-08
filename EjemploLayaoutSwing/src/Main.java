193d61ea4a825c15c0d6e6dd80bc0520da563d7f
Chemislml <chavinchema@hotmail.com>
2023-05-07 18:16:00 -0600

    Actividad extra 4: Ventanas Swing

---
+++ EjemploLayaoutSwing/src/Main.java
@@ -0,0 +1,8 @@
+    //PEREZ MOLINA JOSE MANUEL 2208
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Ventana ventana = new Ventana("Paneles");
+    }
+}
