3849684b419eaf6b671929dfe0e7601304d09746
Chemislml <chavinchema@hotmail.com>
2023-05-14 16:51:50 -0600

    Tarea 15: JTable y TableModel

---
+++ TablaMVC/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+//PEREZ MOLINA JOSE MANUEL 2208
+
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Ventana ventana = new Ventana("Ejemplo de tabla MVC");
+    }
+}
